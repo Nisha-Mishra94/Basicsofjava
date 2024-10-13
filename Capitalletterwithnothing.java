3edee4b98feec4403ac9335a671c540df1deec00
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 13:15:32 +0530

    42 Assignment

---
+++ Capitalletterwithnothing.java
@@ -0,0 +1,13 @@
+package jbasicprogram;
+
+public class Capitalletterwithnothing {
+
+	public static void main(String[] args)
+	{
+	  String m1="Nisha Mishra";
+        String answer=m1.replaceAll("[A-Z]", "");
+        System.out.println(answer);
+
+	}
+
+}
