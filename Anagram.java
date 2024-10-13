3a1a29bab82c7b57efb828d0c4404454d472b877
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 12:44:13 +0530

    35 Assignment

---
+++ Anagram.java
@@ -0,0 +1,37 @@
+package jbasicprogram;
+import java.util.Arrays;
+
+public class Anagram 
+{
+
+	public static void main(String[] args) 
+	{
+		String s1="race";
+		String s2="cafe";
+		if(s1.length()!=s2.length())
+		{
+		System.out.println("They are not anagram");
+		}
+		else
+		{
+		char c1[]= s1.toCharArray();
+		char c2[]= s2.toCharArray();
+
+		Arrays.sort(c1);
+		Arrays.sort(c2);
+		//sorting
+		System.out.println(Arrays.toString(c1));
+		System.out.println(Arrays.toString(c2));
+		boolean b1= Arrays.equals(c1, c2);
+		if(b1==true)
+		{
+		System.out.println("They are anagram");
+		}
+		else
+		{
+		System.out.println("they are not anagram");
+	}
+
+		}
+	}
+}
