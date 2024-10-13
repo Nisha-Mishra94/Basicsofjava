9f6ef036fb8cfbaeeea6bcf77e11af7c1cdafd02
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 16:32:55 +0530

    48 Assignment

---
+++ SpacewithUnderscore.java
@@ -0,0 +1,13 @@
+package jbasicprogram;
+
+public class SpacewithUnderscore {
+
+	public static void main(String[] args) 
+	{
+		String name1="My name is Shyam";
+        String n1=name1.replace("", "_");
+        System.out.println(n1);
+
+	}
+
+}
