57297222ad0aba88709cda7e5fc4ac70a15aee09
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 13:10:15 +0530

    39 Assignment

---
+++ Replacestring1.java
@@ -0,0 +1,13 @@
+package jbasicprogram;
+
+public class Replacestring1 {
+
+	public static void main(String[] args) 
+	{
+		 String n2="I'm in Gurgaon";
+         String n3= n2.replace("Gurgaon", "Delhi");
+         System.out.println(n3);
+
+	}
+
+}
