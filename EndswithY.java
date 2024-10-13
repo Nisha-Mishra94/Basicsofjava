e6289c1d1fc2b36e2ddbbe2fa527ee99a5cfc4af
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 16:24:49 +0530

    46 Assignment

---
+++ EndswithY.java
@@ -0,0 +1,13 @@
+package jbasicprogram;
+
+public class EndswithY {
+
+	public static void main(String[] args) 
+	{
+		String a="I'm a boy";
+		boolean b1=a.matches("(.*)y");
+		System.out.println(b1);
+
+	}
+
+}
