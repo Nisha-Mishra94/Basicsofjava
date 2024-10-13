30331ec9fa193b42cfc54e528d29a24ad1f04ab5
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 13:12:03 +0530

    40 Assignment

---
+++ Replacenothing.java
@@ -0,0 +1,13 @@
+package jbasicprogram;
+
+public class Replacenothing {
+
+	public static void main(String[] args) 
+	{
+		 String name2="kv no 2";
+         String n5=name2.replace("2", "");
+         System.out.println(n5);
+
+	}
+
+}
