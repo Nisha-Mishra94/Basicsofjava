287f17f5d6901cd9ed74fd8721fc6ea70af243c3
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 13:18:15 +0530

    43 Assignment

---
+++ Entirenumeric.java
@@ -0,0 +1,14 @@
+package jbasicprogram;
+
+public class Entirenumeric {
+
+	public static void main(String[] args)
+	{
+		String a1="My home is near to sector 2 Patna 4";
+        String a3=a1.replaceAll("[0-4]", "");
+                System.out.println(a3);
+                }
+
+	}
+
+
