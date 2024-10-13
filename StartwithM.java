590887fffbde0e4bcd0914935c342f4b69b6c781
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 13:54:53 +0530

    45 Assignment

---
+++ StartwithM.java
@@ -0,0 +1,11 @@
+package jbasicprogram;
+
+public class StartwithM {
+
+	public static void main(String[] args) {
+		String a="My name is Ram";
+		boolean b1=a.matches("M(.*)");
+		System.out.println(b1);
+	}
+
+}
