169d61950d57a2e7cf0cb81b6d1116f12f9e75fa
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 17:37:46 +0530

    49 Assignment

---
+++ Nameprint.java
@@ -0,0 +1,30 @@
+package jbasicprogram;
+
+public class Nameprint {
+
+	public static void main(String[] args)
+	{
+		String word = "RAM"; 
+        
+        // Convert the string to a character array 
+        char[] Name = word.toCharArray(); 
+         
+        // Iterate over the character array and print each character 
+        for (char c : Name) { 
+            System.out.println(c); 
+	}
+            String word2 = "MAR"; 
+        
+        // Convert the string to a character array 
+        char[] Name1 = word2.toCharArray(); 
+         
+        // Iterate over the character array and print each character 
+        for (char c1 : Name1) { 
+            System.out.println(c1); 
+
+
+	}
+}
+}
+
+
