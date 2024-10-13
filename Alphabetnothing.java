d079c9343460f177604048a18181bd794045205f
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 13:13:39 +0530

    41 Assignment

---
+++ Alphabetnothing.java
@@ -0,0 +1,14 @@
+package jbasicprogram;
+
+public class Alphabetnothing 
+{
+
+	public static void main(String[] args) 
+	{
+		   String n="kv no 2";
+           String n4=n.replaceAll("[a-z]", "");
+           System.out.println(n4);
+
+	}
+
+}
