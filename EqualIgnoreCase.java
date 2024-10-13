fb0b9576b3a99be3d0a18b18189d91fdea964ac0
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 13:06:03 +0530

    37 Assignment

---
+++ EqualIgnoreCase.java
@@ -0,0 +1,13 @@
+package jbasicprogram;
+
+public class EqualIgnoreCase {
+
+	public static void main(String[] args) 
+	{
+		   String name="Nisha";
+           boolean b1=        name.equalsIgnoreCase("nisha");
+           System.out.println(b1);
+
+	}
+
+}
