4efb9bf7a6c49ce6d0ee6ba05be5bff8ed159db7
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 13:51:31 +0530

    44 Assignment

---
+++ Program2.java
@@ -0,0 +1,13 @@
+package jbasicprogram;
+
+public class Program2 {
+
+	public static void main(String[] args)
+	{
+		String name="cat";
+		boolean b1=name.matches("...");
+		System.out.println(b1);
+
+	}
+
+}
