f9cacc8a64de75240923849815144a70961ac690
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 16:30:12 +0530

    47 Assignment

---
+++ Letterpresentcheck.java
@@ -0,0 +1,12 @@
+package jbasicprogram;
+
+public class Letterpresentcheck {
+
+	public static void main(String[] args) 
+	{
+		String a="I'm a simple boy";
+		boolean b1=a.matches("(.*)s(.*)");
+		System.out.println(b1);
+	}
+
+}
