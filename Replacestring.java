b3c8c3fdd65322c920c86439755dd522c1b5b72a
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-13 13:08:03 +0530

    38 Assignment

---
+++ Replacestring.java
@@ -0,0 +1,13 @@
+package jbasicprogram;
+
+public class Replacestring {
+
+	public static void main(String[] args) 
+	{
+		String name1="nisha";
+        String n1=        name1.replace('n', 'k');
+        System.out.println(n1);
+
+	}
+
+}
