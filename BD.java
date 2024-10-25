addcc600660c6481ebb3508f7d43cba748f850ae
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-25 21:01:00 +0530

    51-72 Assignment

---
+++ BD.java
@@ -0,0 +1,25 @@
+package Outsidepac;
+
+public class BD {
+
+	
+		public void Login()
+		{
+			System.out.println("Hi please login");
+		}
+		protected void Registration()
+		{
+			System.out.println("Hi please Register");
+		}
+		void Fees()
+		
+		{
+			System.out.println("Hi please pay fees");
+		}
+	 private void College()
+	 {
+		 System.out.println("Hi please come college");
+	 }
+	}
+
+
