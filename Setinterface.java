34bf929d33945eb537085992a1c34ef6258b82a1
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-25 21:08:26 +0530

    51-72 Assignment

---
+++ Setinterface.java
@@ -0,0 +1,20 @@
+package jbasicprogram;
+
+import java.io.*;
+import java.util.*;
+
+public class Setinterface {
+
+	
+		public static void main(String[] args)
+	    {
+			Set s1 = new HashSet();
+	        s1.add(56);
+	        s1.add(60);
+	        s1.add(75);
+	        s1.add("nisha");
+	        s1.add('x');
+	        System.out.println(s1);
+	}
+
+}
