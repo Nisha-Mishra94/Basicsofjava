b878afbe9202cd3d56efb11f4ed1971a10d7cffd
Nisha-Mishra94 <nishmishra96@gmail.com>
2024-10-28 18:34:59 +0530

    51-72 Assignment

---
+++ Accessspecifierwithsubclass.java
@@ -0,0 +1,16 @@
+package jbasicprogram;
+
+import Outsidepac.CD;
+
+public class Accessspecifierwithsubclass extends CD
+{
+
+	public static void main(String[] args) 
+	{
+		Accessspecifierwithsubclass as=new Accessspecifierwithsubclass();
+		as.Login();
+		as.Registration();
+		
+	}
+
+}
---
+++ CD.java
@@ -0,0 +1,24 @@
+package Outsidepac;
+
+public class CD 
+{
+	public void Login()
+	{
+		System.out.println("Hi please login");
+	}
+	protected void Registration()
+	{
+		System.out.println("Hi please Register");
+	}
+	void Fees()
+	
+	{
+		System.out.println("Hi please pay fees");
+	}
+ private void College()
+ {
+	 System.out.println("Hi please come college");
+ }
+
+
+}
